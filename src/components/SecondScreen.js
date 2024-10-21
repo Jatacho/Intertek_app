7a6a23578331631b82d582a92d9558afa53c43cf
Jesus Atacho <jesusdavid1510@gmail.com>
2024-10-21 12:42:59 -0600

    Comitiado

--- src/components/SecondScreen.js
+++ src/components/SecondScreen.js
@@ -14,7 +14,7 @@ const SecondScreen = ({ navigation }) => {
   const [minute2, setMinute2] = useState('00');
   const [second2, setSecond2] = useState('00');
  
-const esp8266IP = "http://192.168.95.205";  // Cambia esto con la IP de tu ESP8266
+const esp8266IP = "http://10.224.49.5";  // Cambia esto con la IP de tu ESP8266
  
   // Función para enviar datos a la ESP8266
   const enviarDatos = async () => {
